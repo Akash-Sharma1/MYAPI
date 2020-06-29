6062e6367853aef3919e8ea048260f31b970882c
Akash Sharma <aakashsharmajc100@gmail.com>
2020-06-29 14:22:52 +0530

    Backend hs its own repo

--- Backend/CRUD/app/Templates/generateRandomVar.py
+++
@@ -1,8 +0,0 @@
-import random 
-
-def genRandomstring(length):
-    R = "_"
-    for i in range(0,length):
-        r = chr( random.randrange(97,97+26) )
-        R += r
-    return R
--- Backend/CRUD/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index,name="Index"),
-    path('save',views.Save_user,name="Save"),
-]
